17fcfc0787ca5768063fb4de091ef1394fd471fe
Sai Charan Uvaraj <saicharanuvi01@gmail.com>
2024-02-20 23:08:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function animateJumping()
+{
+    var element = document.getElementById("city");
+    element.classList.add("cardJumpingAnimation");
+}
+function stopJumping()
+{
+    var element = document.getElementById("city");
+    element.classList.remove("cardJumpingAnimation");
+}
